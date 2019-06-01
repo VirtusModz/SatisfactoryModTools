ef5b0f9341fe15b1a008d08751a13a3378283ea7
Tom Ferriere <0theinfinity0@gmail.com>
2019-06-01 11:52:23 +0200

    Release 1.1.2

--- SatisfactoryModToolsCMD/Program.cs
+++ SatisfactoryModToolsCMD/Program.cs
@@ -10,60 +10,84 @@ namespace SatisfactoryModToolsCMD
 {
     class Program
     {
+        public void title()
+        {
+            Console.WriteLine("+----------------------------------------+\r");
+            Console.WriteLine("| Satisfactory Mod Tools for Windows x64 |\r");
+            Console.WriteLine("+----------------------------------------+\n");
+
+            Console.WriteLine("+--------------------------------------------------------------------+\r");
+            Console.WriteLine("| /!\\ You need Unreal Engine 4.20.3 to make this tool work properly! |\r");
+            Console.WriteLine("+--------------------------------------------------------------------+\n\n");
+        }
+
         static void Main(string[] args)
         {
 
-            // Unreal data
-            String PackagerPath = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + @"\Epic Games\UE_4.20\Engine\Binaries\Win64\UnrealPak.exe";
+            Program prgm = new Program();
+
+            // Unreal Data //
+            String DUnrealPath = @"C:\Program Files\Epic Games\UE_4.20";
+            String UnrealPath = null;
             String PakListPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Unreal Engine\AutomationTool\Logs\C+Program+Files+Epic+Games+UE_4.20\PakList_FactoryGame-WindowsNoEditor.txt";
 
-            // Mod data
+            // Mod Data //
+            String DGamePath = @"C:\Program Files\Epic Games\SatisfactoryEarlyAccess";
             String GamePath = null;
             String ModName = null;
 
-            // Package data
+            // Package Data //
             String ResponseFile = @"C:\temp\Response.txt";
             String ResponseParameters = null;
 
-            // I/O Texts
-            Console.WriteLine("Satisfactory Mod Tools for Windows\r");
-            Console.WriteLine("----------------------------------\n\n");
+            prgm.title();
 
-            Console.WriteLine("/!\\ You need Unreal Engine 4.20.3 to make this tool work properly!\n\n");
-            Console.WriteLine("-------------------------------------------------------------------\n\n");
-
-            Console.WriteLine("Type in your Satisfactory game folder, and then press Enter");
-            Console.WriteLine("The default one is: " + @"C:\Program Files\Epic Games\SatisfactoryEarlyAccess");
+            // Game path processing //
+            Console.WriteLine("Type in your Satisfactory game directory path (Blank for default value), and then press Enter");
+            Console.WriteLine("Default value: " + DGamePath);
             GamePath = Convert.ToString(Console.ReadLine());
+            if(String.IsNullOrEmpty(GamePath))
+            {
+                GamePath = DGamePath;
+            }
+
+            // Engine path processing //
+            Console.WriteLine("Type in your UnrealEngine directory path (Blank for default value), and then press Enter");
+            Console.WriteLine("Default value: " + DUnrealPath);
+            UnrealPath = Convert.ToString(Console.ReadLine());
+            if (String.IsNullOrEmpty(UnrealPath))
+            {
+                UnrealPath = DUnrealPath;
+            }
+
+            String PackerPath = UnrealPath + @"\Engine\Binaries\Win64\UnrealPak.exe";
 
             Console.WriteLine("Type in the name of your Mod, and then press Enter");
             ModName = Convert.ToString(Console.ReadLine());
 
-            // Creating Response File
-            Console.WriteLine("Initialize Response File...");
+            Console.Clear();
+            prgm.title();
+
+            // Response File Section //
             String fileName = @"C:\temp\Response.txt";
 
             try
             {
-                // Check if file already exists. If yes, delete it.
+                // Check if file already exists. If yes, delete it. //
                 if (File.Exists(fileName))
                 {
-                    Console.WriteLine("Response File already exist, deleting the old one...");
+                    Console.WriteLine("Response File already exist, removing the old one...");
                     File.Delete(fileName);
                 }
 
-                // Create a new file
+                // Create a new file //
                 using (FileStream fs = File.Create(fileName))
                 {
                     Console.WriteLine("Creating Response File...");
                     Console.WriteLine("Reading PakList File...");
+                    Console.WriteLine("Parsing PakList File...");
 
-                    String text = File.ReadAllText(PakListPath);
-                    Console.WriteLine(text);
-
-                    Console.WriteLine("Parsing required parameters...");
-
-                     // Parse it
+                    // Parse it //
 
                     String[] lines = File.ReadAllLines(PakListPath);
                     IEnumerable<string> selectLines = lines.Where(line => line.Contains(@"\FactoryGame\Saved\Cooked\WindowsNoEditor\FactoryGame\Content\"));
@@ -75,9 +99,9 @@ namespace SatisfactoryModToolsCMD
                     Console.WriteLine("Parsed with success!");
                     Console.WriteLine(ResponseParameters);
 
-                    // Add the parsed data in the file
+                    // Add the parsed data in the file //
 
-                    Console.WriteLine("Writing parsed data into the file...");
+                    Console.WriteLine("Writing parsed data into the new file...");
                     Byte[] content = new UTF8Encoding(true).GetBytes(ResponseParameters);
                     fs.Write(content, 0, content.Length);
                     Console.WriteLine("Done!");
@@ -88,27 +112,31 @@ namespace SatisfactoryModToolsCMD
                 Console.WriteLine(Ex.ToString());
             }
 
-            Console.Clear();
-
             try
             {
-                Console.WriteLine("Packaging the mod...");
-                // Determine whether the directory exists.
+                // Determine whether the directory exists. //
                 if (Directory.Exists(" \"" + GamePath + @"\FactoryGame\Content\Paks\~mods"))
                 {
-                    // If there is already a directory, just create the mod inside
-                    Console.WriteLine("That path exists already. Adding the mod inside...");
+                    // If there is already a directory, just create the mod inside //
+                    Console.WriteLine("Inserting Mod in ~mods directory");
                 }
 
-                // Try to create the directory, the pak patch file, the sig patch file.
+                // Try to create the directory, the pak patch file, the sig patch file. //
                 String path = GamePath + @"\FactoryGame\Content\Paks\~mods\";
+
                 DirectoryInfo di = Directory.CreateDirectory(path);
-                Console.WriteLine("The directory was created successfully at {0}.", Directory.GetCreationTime(path));
-                Process.Start("\"" + PackagerPath + " \"", " \"" + path + ModName + "_p.pak\" -Create=\"" + ResponseFile + "\"");
-                Console.WriteLine("Success!");
-                Console.WriteLine("Adding custom SIG...");
+                Console.WriteLine("~mods directory was successfully created!");
+
+                // Packaging / SIG Creation //
+
+                Console.WriteLine("Creating PAK file...");
+                Process.Start("\"" + PackerPath + "\"", " \"" + path + ModName + "_p.pak\" -Create=\"" + ResponseFile + "\"");
+                Console.WriteLine("Creating SIG file...");
+
                 File.Copy(Path.Combine(GamePath + @"\FactoryGame\Content\Paks", "FactoryGame-WindowsNoEditor.sig"), Path.Combine(GamePath + @"\FactoryGame\Content\Paks\~mods", ModName + "_p.sig"), true);
                 Console.WriteLine("Success!");
+                Console.WriteLine("Success!");
+
             } catch (Exception Ex)
             {
                 Console.Write(Ex.ToString() + "\n");
